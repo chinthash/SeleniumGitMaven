531841e8af35acf3ba676658f4df111c49778be2
Shravan Chintha <schintha@opentext.com>
2021-02-21 14:54:23 +0530

    src commit

---
+++ src/test/java/testpackage/TestClass1.java
@@ -0,0 +1,27 @@
+package testpackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass1 {
+	WebDriver driver;
+
+	@Test
+	public void login() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.name("q")).sendKeys("Shravan", Keys.ENTER);
+		driver.quit();
+	}
+
+}
