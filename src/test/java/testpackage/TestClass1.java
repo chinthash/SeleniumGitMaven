829f3db9e79d09a63f0bffa6795a2049c660b877
Shravan Chintha <schintha@opentext.com>
2021-02-21 23:13:11 +0530

    firefox browser

--- src/test/java/testpackage/TestClass1.java
+++ src/test/java/testpackage/TestClass1.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,8 +15,8 @@ public class TestClass1 {
 
 	@Test
 	public void login() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
